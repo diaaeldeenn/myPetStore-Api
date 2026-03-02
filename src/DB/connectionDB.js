5f3a550b165b7cd7b229f61e8d3b89e55e87dde0
Diaa Eldeen <diaaelseady@gmail.com>
2026-03-02 22:21:08 +0200

    fix deploy

--- src/DB/connectionDB.js
+++ src/DB/connectionDB.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectionDB = async () => {
+  if (isConnected) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log("Connected Successfully");
   } catch (error) {
     console.log("Connection Failed");
--- src/app.controller.js
+++ src/app.controller.js
@@ -8,29 +8,26 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const bootstrap = () => {
-  app.use(cors(), express.json());
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome In My Api" });
-  });
-  connectionDB();
-  app.use("/users", userRouter);
-  app.use("{/*demo}", (req, res) => {
-    throw new Error(`Url ${req.originalUrl} Not Found!`, { cause: 404 });
-  });
-  app.use((err, req, res, next) => {
-    res
-      .status(err.cause || 500)
-      .json({ message: err.message, stack: err.stack });
-  });
+app.use(async (req, res, next) => {
+  await connectionDB();
+  next();
+});
 
+app.use(cors(), express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome In My Api" });
+});
+app.use("/users", userRouter);
+app.use("{/*demo}", (req, res) => {
+  throw new Error(`Url ${req.originalUrl} Not Found!`, { cause: 404 });
+});
+app.use((err, req, res, next) => {
+  res.status(err.cause || 500).json({ message: err.message, stack: err.stack });
+});
 
-  if (process.env.NODE_ENV !== "production") {
-    app.listen(port, () => console.log(`Sever Work On Port ${port}!`));
-  }
-};
-
-bootstrap();
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => console.log(`Sever Work On Port ${port}!`));
+}
 
 export default app;
