655eea10fb54528d8c996d75bc4e135aa8e49bf8
Diaa Eldeen <diaaelseady@gmail.com>
2026-04-15 12:07:52 +0200

    fix:deleteAllwishlist

--- src/modules/wishlist/wishlist.controller.js
+++ src/modules/wishlist/wishlist.controller.js
@@ -6,8 +6,8 @@ import * as WL from "./wishlist.service.js";
 const wishlistRouter = Router();
 
 wishlistRouter.post("/", authentication, WL.addToWishlist);
+wishlistRouter.delete("/", authentication, WL.clearWishlist);
 wishlistRouter.delete("/:productId", authentication, WL.removeFromWishlist);
 wishlistRouter.get("/", authentication,WL.getWishlist);
-wishlistRouter.delete("/wishlist", authentication, WL.clearWishlist);
 
 export default wishlistRouter;
